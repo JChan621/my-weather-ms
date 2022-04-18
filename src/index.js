5d931ce5bd21f8fbb7a16dd2baae9ca9e23f421f
fourierz517 <83400846+fourierz517@users.noreply.github.com>
2022-04-18 12:40:02 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+fetch('https://city-trie-route.herokuapp.com', {mode: 'cors'});
 ReactDOM.render(
   <React.StrictMode>
     <App />
